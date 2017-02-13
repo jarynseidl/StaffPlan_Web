dbd951ca90c8f7cfc183cb35f6ebfc6556e3ec3c
jarynseidl <jarynseidl@gmail.com>
2017-02-13 15:36:46 -0700

    Add project files.

---
+++ StaffPlan_Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StaffPlan_Web.MvcApplication" Language="C#" %>
---
+++ StaffPlan_Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StaffPlan_Web.Startup))]
+namespace StaffPlan_Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
